bce47d546060188d3c53904e8c0c363ce6330ff0
Carl Pearson <cap10@ufl.edu>
2016-06-26 13:39:19 -0400

    pbs generator for pc runs

---
+++ pc-detect.sh
@@ -0,0 +1,18 @@
+cat <<EOF
+#!/bin/bash
+#PBS -r n
+#PBS -N $1
+#PBS -o $1.o
+#PBS -e $1.err
+#PBS -m a
+#PBS -M cap10@ufl.edu
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l pmem=4gb
+#PBS -t 1-$3
+
+module load gcc/5.2.0 R/3.2.2
+cd /scratch/lfs/cap10/montreal-detect
+tar=\$(printf 'input/detection/$2/%03d/pc.rds' \$PBS_ARRAYID)
+make \$tar
+EOF
